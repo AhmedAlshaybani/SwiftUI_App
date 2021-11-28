2781d214ea9dda6453166b7148d31749bc168310
Dev.ahmed <Ahmed Alsheepo>
2021-11-28 14:48:10 +0200

    added a model file to structure the fetched data

---
+++ SwiftUI App/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  SwiftUI App
+//
+//  Created by Dev.ahmed on 11/28/21.
+//
+
+import SwiftUI
+
+
+struct modelData {
+    
+    var name: String
+    var age: Int
+    
+    
+    
+}

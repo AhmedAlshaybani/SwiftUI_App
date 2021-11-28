4845b4fa0e6a45b6385e36799587e0d93950fde3
Dev.ahmed <Ahmed Alsheepo>
2021-11-28 14:27:51 +0200

    initial state of app

---
+++ SwiftUI App/SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppApp.swift
+//  SwiftUI App
+//
+//  Created by Dev.ahmed on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

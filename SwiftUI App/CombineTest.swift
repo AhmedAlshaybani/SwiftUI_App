26d38b0fc9a66300a55ec2dce226dac884fc5f41
Dev.ahmed <Ahmed Alsheepo>
2021-11-29 21:10:08 +0200

    adding a combine basic knowledge

--- SwiftUI App/CombineTest.swift
+++ SwiftUI App/CombineTest.swift
@@ -8,4 +8,16 @@
 import SwiftUI
 import Combine
 
+let productPrice = CurrentValueSubject<Int, Never>(15)
+let userBalance = CurrentValueSubject<Int, Never>(10)
+var isPossible = false
 
+let canMakePurchase = productPrice
+    .combineLatest(userBalance)
+    .sink {
+       isPossible = $0 > $1
+    }
+
+print(isPossible)
+    
+    
